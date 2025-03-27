9c6378d650bb1865a64960823af3a0a2b28cf992
janpc01 <janpc01@gmail.com>
2025-03-27 12:34:58 -0600

    Migrated db

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,9 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[8.0].define(version: 2025_03_26_193708) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "pg_catalog.plpgsql"
+
   create_table "boards", force: :cascade do |t|
     t.string "title"
     t.integer "user_id", null: false
